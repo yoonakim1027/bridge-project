b905a01acfea54954217f337a0a0790702cec932
ynkim <ynkim@rexgen.co.kr>
2023-10-16 19:51:45 +0900

    Feature: 업로드 페이지 폴더, 기본 레이아웃 설정
    김유나(23.10.16)
    
    태그: 제목
    :(space)제목 으로 :뒤에만 space를 넣는다.
    
    Feature:	새로운 기능을 추가
    Fix:	버그 수정
    Design:	CSS 등 사용자 UI 디자인 변경
    !BREAKING CHANGE:	커다란 API 변경의 경우
    !HOTFIX:	급하게 치명적인 버그를 고쳐야하는 경우
    Style:	코드 포맷 변경, 세미 콜론 누락, 코드 수정이 없는 경우
    Refactor:	프로덕션 코드 리팩토링
    Comment:	필요한 주석 추가 및 변경
    Docs:	문서 수정
    Test:	테스트 코드, 리펙토링 테스트 코드 추가, Production Code(실제로 사용하는 코드) 변경 없음
    Chore:	빌드 업무 수정, 패키지 매니저 수정, 패키지 관리자 구성 등 업데이트, Production Code 변경 없음
    Rename:	파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
    Remove:	파일을 삭제하는 작업만 수행한 경우

---
+++ src/pages/Uploaders/ProviderInfo/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Header from '@/components/Login/Delivery/Header';
+import InputField from '@/components/Login/Delivery/InputField';
+import ImageUploader from '@/components/Login/Delivery/ImageUploader';
+import SubmitButton from '@/components/Login/Delivery/SubmitButton';
+import CancelButton from '@/components/Login/Delivery/CancelButton';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+const ProviderInfoPage: React.FC = () => {
+  const handleCancel = () => {
+    console.log('Cancelled');
+  };
+
+  return (
+    <Container>
+      <Header title="탁송 공급자 정보 입력" />
+
+      <Box component="form" noValidate autoComplete="off" mt={2}>
+        <InputField label="이름" placeholder="이름을 입력하세요." />
+        <InputField label="연락처" placeholder="연락처를 입력하세요." />
+        <InputField label="주소" placeholder="주소를 입력하세요." />
+
+        <Box mt={2}>
+          <ImageUploader label="사진 업로드" />
+        </Box>
+
+        <Box mt={2}>
+          <SubmitButton text="제출" />
+          <CancelButton text="취소" onClick={handleCancel} />
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default ProviderInfoPage;
