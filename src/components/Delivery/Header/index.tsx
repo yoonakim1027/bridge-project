d1b814045deea590b024b262ceb70ce245790040
ynkim <ynkim@rexgen.co.kr>
2023-10-17 17:53:44 +0900

    Fix: 스타일 수정
    김유나(23.10.17)
    
    태그: 제목
    :(space)제목 으로 :뒤에만 space를 넣는다.
    
    Feature:	새로운 기능을 추가
    Fix:	버그 수정
    Design:	CSS 등 사용자 UI 디자인 변경
    !BREAKING CHANGE:	커다란 API 변경의 경우
    !HOTFIX:	급하게 치명적인 버그를 고쳐야하는 경우
    Style:	코드 포맷 변경, 세미 콜론 누락, 코드 수정이 없는 경우
    Refactor:	프로덕션 코드 리팩토링
    Comment:	필요한 주석 추가 및 변경
    Docs:	문서 수정
    Test:	테스트 코드, 리펙토링 테스트 코드 추가, Production Code(실제로 사용하는 코드) 변경 없음
    Chore:	빌드 업무 수정, 패키지 매니저 수정, 패키지 관리자 구성 등 업데이트, Production Code 변경 없음
    Rename:	파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
    Remove:	파일을 삭제하는 작업만 수행한 경우

--- src/components/Delivery/Header/index.tsx
+++ src/components/Delivery/Header/index.tsx
@@ -2,18 +2,38 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { styled } from '@mui/system';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 interface HeaderProps {
   title: string;
 }
 
+const CustomAppBar = styled(AppBar)({
+  background:
+    'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)',
+  boxShadow: '0px 3px 15px rgba(0, 0, 0, 0.2)',
+});
+
+const StyledTypography = styled(Typography)({
+  flexGrow: 1, // Takes the rest of the available space
+  color: '#fff',
+  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)',
+  marginLeft: '10px',
+});
+
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
-    <AppBar position="static">
+    <CustomAppBar position="static">
       <Toolbar>
-        <Typography variant="h6">{title}</Typography>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <StyledTypography variant="h6">{title}</StyledTypography>
+        {/* You can add more icons or buttons here */}
       </Toolbar>
-    </AppBar>
+    </CustomAppBar>
   );
 };
 
