7d91f48b459eb4c5d0e49700d04beeb6e21f8e59
ynkim <ynkim@rexgen.co.kr>
2023-10-26 13:57:56 +0900

    Fix: 호출하면 오는 데이터로 띄우도록 변경
    김유나(23.10.26)
    
    태그: 제목
    :(space)제목 으로 :뒤에만 space를 넣는다.
    
    Feature:	새로운 기능을 추가
    Fix:	버그 수정
    Design:	CSS 등 사용자 UI 디자인 변경
    !BREAKING CHANGE:	커다란 API 변경의 경우
    !HOTFIX:	급하게 치명적인 버그를 고쳐야하는 경우
    Style:	코드 포맷 변경, 세미 콜론 누락, 코드 수정이 없는 경우
    Refactor:	프로덕션 코드 리팩토링
    Comment:	필요한 주석 추가 및 변경
    Docs:	문서 수정
    Test:	테스트 코드, 리펙토링 테스트 코드 추가, Production Code(실제로 사용하는 코드) 변경 없음
    Chore:	빌드 업무 수정, 패키지 매니저 수정, 패키지 관리자 구성 등 업데이트, Production Code 변경 없음
    Rename:	파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
    Remove:	파일을 삭제하는 작업만 수행한 경우

--- src/components/Request/index.tsx
+++ src/components/Request/index.tsx
@@ -1,5 +1,5 @@
 // RequestForm.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Container,
   Box,
@@ -16,36 +16,86 @@ import { RequestorInfo } from './RequestInfo';
 import { PickupInfo } from './PickupInfo';
 import { DeliveryInfo } from './DeliveryInfo';
 
+type Requestor = {
+  name: string;
+  contact: string;
+  address: string;
+};
+
+type Pickup = {
+  address: string;
+  datetime: string;
+  contact: string;
+};
+
+type Delivery = {
+  address: string;
+  datetime: string;
+  recipientName: string;
+  recipientContact: string;
+};
+
+type RequestData = {
+  requestor: Requestor;
+  pickup: Pickup;
+  delivery: Delivery;
+};
+
+const DUMMY_DATA: RequestData[] = [
+  {
+    requestor: {
+      name: '홍길동',
+      contact: '010-1234-5678',
+      address: '서울시 강남구',
+    },
+    pickup: {
+      address: '서울시 강남구',
+      datetime: '2023-11-01 14:00',
+      contact: '010-1234-5678',
+    },
+    delivery: {
+      address: '서울시 서초구',
+      datetime: '2023-11-01 16:00',
+      recipientName: '김철수',
+      recipientContact: '010-9876-5432',
+    },
+  },
+  // ... 추가적인 데이터 항목들
+];
+
 export const RequestForm: React.FC = () => {
+  const [data, setData] = useState<RequestData[]>(DUMMY_DATA);
+
+  useEffect(() => {
+    // 여기에서 API를 호출합니다.
+    async function fetchData() {
+      try {
+        const response = await fetch('YOUR_API_ENDPOINT');
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    }
+
+    // fetchData(); // API 호출이 준비되면 이 주석을 해제
+  }, []);
+
   return (
     <Container maxWidth="md">
-      <Paper sx={{ p: 3, m: 4 }}>
-        <Typography sx={{ textAlign: 'center' }} component="h1" variant="h4">
-          주문서 상세 페이지
-        </Typography>
-        <Box my={2}>
-          <RequestorInfo
-            name="홍길동"
-            contact="010-1234-5678"
-            address="서울시 강남구"
-          />
+      {data.map((item, index) => (
+        <Box key={index} mb={4}>
+          <Box my={2}>
+            <RequestorInfo {...item.requestor} />
+          </Box>
+          <Box my={2}>
+            <PickupInfo {...item.pickup} />
+          </Box>
+          <Box my={2}>
+            <DeliveryInfo {...item.delivery} />
+          </Box>
         </Box>
-        <Box my={2} sx={{ mt: 4 }}>
-          <PickupInfo
-            address="서울시 강남구"
-            datetime="2023-11-01 14:00"
-            contact="010-1234-5678"
-          />
-        </Box>
-        <Box my={2} sx={{ mt: 4 }}>
-          <DeliveryInfo
-            address="서울시 서초구"
-            datetime="2023-11-01 16:00"
-            recipientName="김철수"
-            recipientContact="010-9876-5432"
-          />
-        </Box>
-      </Paper>
+      ))}
     </Container>
   );
 };
